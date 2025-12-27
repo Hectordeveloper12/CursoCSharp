4b1b02c44445e20d1a3a9b3310fc379f6b9bed31
hector pereira martins <hectorpereiralimamartins1@gmail.com>
2025-12-27 18:32:07 -0300

    adicionando aulas

---
+++ Exercicio8/Exercicio8/Banco.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+
+namespace Exercicio8
+{
+    internal class Banco
+    {
+        public int Id { get; private set; }
+        //Não pode ser mudado
+        public string Name { get; set; }
+        //Só pode ser adicionado depósito atravéz de um metodo
+        public double Deposito { get; private set;}
+
+        public Banco() { }
+
+        public Banco(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Deposito = 0;
+        }
+
+        public Banco(int id, string name, double depositoInicial) :this(id, name)
+        {
+            Depositar(depositoInicial);
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+
+        public double Depositar(double deposito)
+        {
+            return Deposito += deposito;
+        }
+
+        public double Sacar(double deposito)
+        {
+            return Deposito -= (deposito + 5);
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Id + ", Titular: " + Nome + ", Saldo: " + Deposito.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio8/Exercicio8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio8
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+
+            Console.Write("Entre o número da conta: ");
+            int idConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito incial (s/n): ");
+            string escolha = Console.ReadLine();
+
+            Banco p;
+
+            if (escolha == "s")
+            {
+                Console.Write("Entre o valor de depósito inical: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                p = new Banco(idConta, nome, depositoInicial);
+            }else
+            {
+                p = new Banco(idConta, nome);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(p);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double depositando = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.Depositar(depositando);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(p);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor de saque: ");
+            double sacando = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.Sacar(sacando);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(p);
+        }
+    }
+}
